9bafd54179faa20ecca2925bed462d191ca446fa
tjoleary56 <tjoleary@umass.edu>
2017-08-16 11:09:34 -0400

    Update welcome.php

--- welcome.php
+++ welcome.php
@@ -2,11 +2,11 @@
 $emailErr = "";
 $email = "";
   
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  if (empty($_POST["email"])) {
+if ($_SERVER["REQUEST_METHOD"] == "GET") {
+  if (empty($_GET["email"])) {
     $emailErr = "Email is required";
   } else {
-    $email = test_input($_POST["email"]);
+    $email = test_input($_GET["email"]);
     if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
       $emailErr = "Invalid email format"; 
     }
